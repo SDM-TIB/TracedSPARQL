b8215a2d1029141a4b08cb799b665ffa0169f7cd
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-01-29 09:52:44 +0100

    add safeguard for result analysis
    remove summarized results and plots before creating them anew

--- 06_plots.sh
+++ 06_plots.sh
@@ -2,5 +2,7 @@
 # summarizing results and preparing plots
 
 docker build ./analysis -t sdmtib/tracedsparql:analysis
+rm -rf results/plots_violin
+rm -rf results/summarized
 docker run -it --rm -v ./results:/results sdmtib/tracedsparql:analysis
 docker image rm sdmtib/tracedsparql:analysis
